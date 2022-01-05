12c1c368a8b6ab26d69f0118f9e616a24d4265f9
Abhinandan Chakraborty <chakraborty.abhinandan@gmail.com>
2022-01-05 04:44:51 +0100

    Clean code

--- main.py
+++ main.py
@@ -11,8 +11,4 @@ def print_hi(name):
 
 if __name__ == '__main__':
     print_hi('PyCharm')
-    print_hi("Hello")
-    print("")
-
-    
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
